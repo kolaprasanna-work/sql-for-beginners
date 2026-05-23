da3cb394adbe2b190458155e8962820be632cfbd
kola-prasanna <kolaprasanna.work@gmail.com>
2026-05-23 16:22:10 +0530

    ✅ Add solution for: Total Revenue Per Customer - SQL

---
+++ exercises/TotalRevenuePerCustomerSQL.sql
@@ -0,0 +1,14 @@
+-- Write a SQL query below --
+SELECT 
+    c.first_name, 
+    c.last_name, 
+   round( SUM(o.total_amount),2) AS total_revenue
+FROM 
+    customers c
+INNER JOIN 
+    orders o 
+    ON c.id = o.customer_id
+GROUP BY 
+    c.id, c.first_name, c.last_name
+ORDER BY 
+    total_revenue DESC;
